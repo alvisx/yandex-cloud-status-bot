c2fd4d2e0b9d86e7c0f26d633471e00dd9c8c613
alvisx <bedinkv@gmail.com>
2020-02-07 20:56:52 +0300

    Test datafiles added and unresolved incidents processing updated

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 (async () => {
+    const fs = require('fs')
     const axios = require('axios')
     const dotenv = require('dotenv')
     const moment = require('moment')
@@ -10,33 +11,56 @@
 
     // Array for unresolved incidents
     const unresolvedIncidents = []
+    let testCounter = 0
 
     setInterval(async () => {
+        bot.start((ctx) => {
+            console.log(ctx.chat.id)
+        })
+        await bot.launch()
+
+        let services = {}
+
         try {
-            const services = (await axios.get('https://status.cloud.yandex.ru/api/services')).data
+
+            // If TEST mode activated then get data from test datafiles
+            if (process.env.TEST) {
+                testCounter++
+                console.log('Test data', testCounter)
+                services = JSON.parse(fs.readFileSync('./test/testData' + testCounter + '.json', 'utf8'))
+                testCounter = testCounter === 5 ? 0 : testCounter
+            } else {
+                services = (await axios.get('https://status.cloud.yandex.ru/api/services')).data
+            }
 
             for (const service of services) {
                 for (const incident of service.incidents) {
 
                     // Try to find incident in unresolved incidents list
-                    const foundIncident = unresolvedIncidents.find(_incident => _incident.id === incident.id)
+                    const foundIncident = unresolvedIncidents.find(
+                        _incident => (_incident.id === incident.id) && (_incident.serviceId === service.id)
+                    )
 
-                    // If incident found him statuses updated send message
-                    if (foundIncident && (foundIncident.status !== incident.status)) {
-                        await bot.telegram.sendMessage(
-                            process.env.CHAT_ID,
-                            "Service " + service.name + "\n" +
-                            incident.title + "\n" +
-                            "Status: " + incident.status + "\n" +
-                            "Level: " + incident.level + "\n" +
-                            "Start Date: " + incident.startDate + "\n" +
-                            "End Date: " + incident.endDate + "\n" +
-                            "status.cloud.yandex.ru"
-                        )
+                    // If incident found and him statuses updated send message
+                    if (foundIncident) {
+                        if (foundIncident.status !== incident.status) {
+                            await bot.telegram.sendMessage(
+                                process.env.CHAT_ID,
+                                "Service " + service.name + "\n" +
+                                incident.title + "\n" +
+                                "Status: " + incident.status + "\n" +
+                                "Level: " + incident.level + "\n" +
+                                "Start Date: " + incident.startDate + "\n" +
+                                "End Date: " + incident.endDate + "\n" +
+                                "status.cloud.yandex.ru"
+                            )
+                        }
 
                         // If incident status updated to 'resolved' delete it from unresolved incidents list
                         if (incident.status === 'resolved') {
-                            const foundIncidentIdx = unresolvedIncidents.find(_incident => _incident.id === incident.id)
+                            const foundIncidentIdx = unresolvedIncidents.find(
+                                _incident => (_incident.id === incident.id) && (_incident.serviceId === service.id)
+                            )
                             unresolvedIncidents.splice(foundIncidentIdx, 1)
                         }
                     } else {
@@ -44,7 +68,7 @@
                         // If incident NOT found in unresolved incidents list and him status is not "resolved"
                         // add it to unresolved incidents list and send message
                         if (incident.status !== 'resolved') {
-                            unresolvedIncidents.push(incident)
+                            unresolvedIncidents.push({...incident, serviceName: service.name, serviceId: service.id})
                             console.log("New incident:\n", incident)
 
                             await bot.telegram.sendMessage(
